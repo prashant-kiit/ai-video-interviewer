5588d147b232b4846e2690c3dbd0d1d378fcd6a4
prashant-m4 <prashantsingh090798@gmail.com>
2026-01-01 19:43:20 +0530

    feat: added hashed password

--- videochat-service/internal/service/user_service.go
+++ videochat-service/internal/service/user_service.go
@@ -9,10 +9,19 @@ import (
 	"github.com/go-playground/validator/v10"
 	"github.com/prashant-kiit/ai-video-interviewer/videochat-service/internal/model"
 	"github.com/redis/go-redis/v9"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type UserService struct {
 	Redis *redis.Client
+}
+
+func (u *UserService) hashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(bytes), nil
 }
 
 func (u *UserService) Validate(w http.ResponseWriter, r *http.Request) (model.SignUpRequest, error) {
@@ -35,12 +44,17 @@ func (u *UserService) CreateUser(w http.ResponseWriter, r *http.Request, req mod
 	if err != nil {
 		return model.SignUpResponse{}, errors.New("failed to generate user id")
 	}
-
+	
+	hashedPassword, err := u.hashPassword(req.Password)
+	if err != nil {
+		return model.SignUpResponse{}, errors.New("failed to hash password")
+	}
+	
 	user := model.User{
 		ID:       strconv.FormatInt(id, 10),
 		Name:     req.Name,
 		Username: req.Username,
-		Password: req.Password,
+		Password: hashedPassword,
 	}
 
 	key := "user:" + user.ID
@@ -56,13 +70,3 @@ func (u *UserService) CreateUser(w http.ResponseWriter, r *http.Request, req mod
 
 	return resp, nil
 }
-
-// func (u *User) HashPassword() error {
-// 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	u.Password = string(hashedPassword)
-// 	return nil
-// }
-
