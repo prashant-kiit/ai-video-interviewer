9ff8944f24cfa8a683a148ba0f9e2b2912fdeb26
prashant-m4 <prashantsingh090798@gmail.com>
2026-01-02 23:04:52 +0530

    feat: added cached token

--- videochat-frontend-app/shared/store/token.ts
+++ videochat-frontend-app/shared/store/token.ts
@@ -1,8 +1,16 @@
+import { atom, useAtom } from "jotai";
+
+export const tokenAtom = atom<string | "">("");
+
 export const useToken = () => {
+  const [cachedToken, setCachedToken] = useAtom(tokenAtom);
   const tokenKey = process.env.NEXT_PUBLIC_TOKEN_KEY as string;
   
   const getToken = () => {
-    return localStorage.getItem(tokenKey) || "";
+    if(cachedToken) return cachedToken;
+    const token = localStorage.getItem(tokenKey) || "";
+    setCachedToken(token);
+    return token;
   };
 
   const storeToken = (newToken: string) => {
@@ -11,6 +19,7 @@ export const useToken = () => {
 
   const removeToken = () => {
     localStorage.removeItem(tokenKey);
+    setCachedToken("");
   };
 
   return { getToken, storeToken, removeToken };
