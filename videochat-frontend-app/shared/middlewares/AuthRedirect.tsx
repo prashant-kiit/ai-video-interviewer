f1afde0b6860a8e7a55f84bee752fb5e01e7e6e9
prashant-m4 <prashantsingh090798@gmail.com>
2026-01-04 12:45:12 +0530

    clean

--- videochat-frontend-app/shared/middlewares/AuthRedirect.tsx
+++ videochat-frontend-app/shared/middlewares/AuthRedirect.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Children, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { Loader } from "../components/Loader";
 import { useAuth, AuthenticationState } from "../hooks/useAuth";
